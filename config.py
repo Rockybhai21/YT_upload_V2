c61407681560000cdcba761ae92aff37b6b52054
Rockybhai21 <rockybhai2109rs@gmail.com>
2025-01-30 02:23:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # set your telegram bot token
-BOT_TOKEN = ""
+BOT_TOKEN = "7207793925:AAH7j5TJJV0CAwAwih38jL0TsUcC710Dx9c"
 tutorial = "BAACAgIAAxkBAAIGQmPrxt24nrWaOytCgu-kUDcUHLseAAK7HwACMh1hS9ol-KxNRCjCLgQ"
